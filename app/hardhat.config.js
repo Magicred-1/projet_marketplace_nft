af5f187e1f1ab8ca65b54e07fee47fd6e84dea6e
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2023-02-21 17:55:57 +0100

    Adding assets

--- app/hardhat.config.js
+++ app/hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.alchemyapi.io/v2/your-api-key",
+        blockNumber: 12965000,
+      },
+    },
+  },
 };
